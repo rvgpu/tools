1280179c1409daa473951323fce281c71142b216
Qi Hangkong <qihangkong@outlook.com>
2023-11-28 17:40:33 +0800

    vm: add vnc to start vm

--- vm/start.sh
+++ vm/start.sh
@@ -1,3 +1,5 @@
+image_path=/home/pub/vm/ubuntu2204_zac.img
+
 absolute_path=$(readlink -f "$0")
 curr_dir=$(dirname "$absolute_path")
 
@@ -7,4 +9,4 @@ export PATH=${rvgpu_top_path}/install/bin:${PATH}
 export LD_LIBRARY_PATH=${rvgpu_top_path}/install/lib
 
 # start qemu use from image
-qemu-system-x86_64 -enable-kvm -m 16G -smp 8 -hda ./ubuntu2204_zac.img --device rvgpu,id=rvg0 --device rvgpu,id=rvg1
+qemu-system-x86_64 -enable-kvm -m 16G -smp 8 -hda ${image_path} --device rvgpu,id=rvg0 --device rvgpu,id=rvg1 -vnc :50
